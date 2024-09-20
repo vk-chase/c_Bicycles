e3edc9e818fb03fa2a10cc9f68003794aabc4274
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2024-09-20 02:41:02 -0500

    Update bmx.lua
    merge all bikes to 1 client file.

--- client/bmx.lua
+++ client/bmx.lua
@@ -1,95 +1,95 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local IsRenting = false
-local notifytype = qb  -- qb  or  rtx
+local notifytype = "qb"  -- "qb" or "rtx"
 
-RegisterNetEvent('c_Bicycle:client:BMXMenu')
-AddEventHandler('c_Bicycle:client:BMXMenu', function()
-    if IsRenting == true then
-        exports['qb-menu']:openMenu({
-            {
-                id = 1,
-                header = 'Bicycle Menu',
-                isMenuHeader = true,
-            },
-            {
-                id = 2,
-                header = "Pack BMX",
-                params = {
-                    event = "c_Bicycle:client:PackBMX"
-                }
-            },
-            {
-                id = 3,
-                header = "< Close",
-                params = {
-                    event = "qb-menu:closeMenu",
-                }
-            },
-        })
-    end
-    if IsRenting == false then
-        exports['qb-menu']:openMenu({
-            {
-                id = 1,
-                header = 'Bicycle Menu',
-                isMenuHeader = true,
-            },
-            {
-                id = 2,
-                header = "Place BMX",
-                params = {
-                    event = "c_Bicycle:client:BMX",
-                    args = {
-                        vehicle = 'bmx',
+local function OpenBicycleMenu(isRenting, placeEvent, packEvent, vehicleName)
+    local menuItems = {
+        {
+            id = 1,
+            header = 'Bicycle Menu',
+            isMenuHeader = true,
+        },
+        {
+            id = 2,
+            header = isRenting and ("Pack " .. vehicleName) or ("Place " .. vehicleName),
+            params = {
+                event = isRenting and packEvent or placeEvent,
+                args = not isRenting and { vehicle = string.lower(vehicleName) } or nil
+            }
+        },
+        {
+            id = 3,
+            header = "< Close",
+            params = {
+                event = "qb-menu:closeMenu",
+            }
+        },
+    }
+    exports['qb-menu']:openMenu(menuItems)
+end
 
-                    }
-                }
-            },
-            {
-                id = 5,
-                header = "< Close",
-                params = {
-                    event = "qb-menu:closeMenu",
-                }
-            },
-        })
-    end
-end)
-
-RegisterNetEvent('c_Bicycle:client:BMX')
-AddEventHandler('c_Bicycle:client:BMX', function(rental)
-    local vehicle = rental.vehicle
+local function PlaceBicycle(data)
+    local vehicle = data.vehicle
     local ped = PlayerPedId()
     local pedCoords = GetEntityCoords(ped)
     local bikeCoords = vector3(pedCoords.x + 2, pedCoords.y, pedCoords.z)
+
     DoScreenFadeOut(1000)
     Wait(1500)
+
     QBCore.Functions.SpawnVehicle(vehicle, function(veh)
         SetVehicleNumberPlateText(veh, "BIKE"..tostring(math.random(1000, 9999)))
-        --exports['LegacyFuel']:SetFuel(veh, 100.0)
-        TaskWarpPedIntoVehicle(GetPlayerPed(-1), veh, -1)
+        TaskWarpPedIntoVehicle(ped, veh, -1)
         TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(veh))
-        if notifytype == rtx then
-            TriggerEvent("rtx_notify:Notify", "Bike Placed", "Bicycle Placed, Use the bike again to pack it up!.", 5000, "success")
+        
+        local message = "Bicycle Placed, Use the bike again to pack it up!"
+        if notifytype == "rtx" then
+            TriggerEvent("rtx_notify:Notify", "Bike Placed", message, 5000, "success")
         else
-            TriggerEvent("QBCore:Notify", "Bicycle Placed, Use the bike again to pack it up!", "success")
+            TriggerEvent("QBCore:Notify", message, "success")
         end
+        
         SetVehicleEngineOn(veh, true, true)
     end, bikeCoords, true) 
-    Citizen.Wait(300)
+
+    Wait(300)
     DoScreenFadeIn(2500)
     IsRenting = true
-end)
+end
 
-RegisterNetEvent('c_Bicycle:client:PackBMX')
-AddEventHandler('c_Bicycle:client:PackBMX', function()
-    if notifytype == rtx then
-        TriggerEvent("rtx_notify:Notify", "Bike Stoed", "Bike Packed.", 5000, "success")
+local function PackBicycle()
+    local message = "Bike Packed!"
+    if notifytype == "rtx" then
+        TriggerEvent("rtx_notify:Notify", "Bike Stored", message, 5000, "success")
     else
-        TriggerEvent("QBCore:Notify", "Bike Packed!", "success")
+        TriggerEvent("QBCore:Notify", message, "success")
     end
-    local car = GetVehiclePedIsIn(PlayerPedId(),true)
+
+    local car = GetVehiclePedIsIn(PlayerPedId(), true)
     DeleteVehicle(car)
     DeleteEntity(car)
     IsRenting = false
-end)
+end
+
+local bicycles = {
+    {name = "BMX", model = "bmx"},
+    {name = "Cruiser", model = "cruiser"},
+    {name = "Fixter", model = "fixter"},
+    {name = "Scorcher", model = "scorcher"},
+    {name = "Whippet Race Bike", model = "tribike"},
+    {name = "Endurex Race Bike", model = "tribike2"},
+    {name = "Tri-Cycles Race Bike", model = "tribike3"}
+}
+
+for _, bike in ipairs(bicycles) do
+    RegisterNetEvent('c_Bicycle:client:' .. bike.name .. 'Menu')
+    AddEventHandler('c_Bicycle:client:' .. bike.name .. 'Menu', function()
+        OpenBicycleMenu(IsRenting, 'c_Bicycle:client:' .. bike.name, 'c_Bicycle:client:Pack' .. bike.name, bike.name)
+    end)
+
+    RegisterNetEvent('c_Bicycle:client:' .. bike.name)
+    AddEventHandler('c_Bicycle:client:' .. bike.name, PlaceBicycle)
+
+    RegisterNetEvent('c_Bicycle:client:Pack' .. bike.name)
+    AddEventHandler('c_Bicycle:client:Pack' .. bike.name, PackBicycle)
+end
