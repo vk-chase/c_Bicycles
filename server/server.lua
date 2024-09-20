c60d585542f6eb0a9a42207569dfac575fc41104
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2024-09-20 02:40:23 -0500

    Update server.lua
    fixed code duplication. 
    made bike table.
    
    attempting sting manipulation for force capital client.

--- server/server.lua
+++ server/server.lua
@@ -1,44 +1,24 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Functions.CreateUseableItem("bmx", function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-         TriggerClientEvent('c_Bicycle:client:BMXMenu', source)
-    end
-end)
-QBCore.Functions.CreateUseableItem("cruiser", function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-        TriggerClientEvent('c_Bicycle:client:CruiserMenu', source)
-    end
-end)
-QBCore.Functions.CreateUseableItem("fixter", function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-        TriggerClientEvent('c_Bicycle:client:FixterMenu', source)
-    end
-end)
-QBCore.Functions.CreateUseableItem("scorcher", function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-        TriggerClientEvent('c_Bicycle:client:ScorcherMenu', source)
-    end
-end)
-QBCore.Functions.CreateUseableItem("tribike", function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-        TriggerClientEvent('c_Bicycle:client:TribikeMenu', source)
-    end
-end)
-QBCore.Functions.CreateUseableItem("tribike2", function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-        TriggerClientEvent('c_Bicycle:client:Tribike2Menu', source)
-    end
-end)
-QBCore.Functions.CreateUseableItem("tribike3", function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-        TriggerClientEvent('c_Bicycle:client:Tribike3Menu', source)
-    end
-end)
+local bicycles = {
+    "bmx",
+    "cruiser",
+    "fixter",
+    "scorcher",
+    "tribike",
+    "tribike2",
+    "tribike3"
+}
+
+local function CreateBicycleItem(bicycleName)
+    QBCore.Functions.CreateUseableItem(bicycleName, function(source, item)
+        local Player = QBCore.Functions.GetPlayer(source)
+        if Player.Functions.GetItemBySlot(item.slot) ~= nil then
+            TriggerClientEvent('c_Bicycle:client:' .. bicycleName:gsub("^%l", string.upper) .. 'Menu', source)
+        end
+    end)
+end
+
+for _, bicycle in ipairs(bicycles) do
+    CreateBicycleItem(bicycle)
+end
