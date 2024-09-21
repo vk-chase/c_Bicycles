6e47b092f343e4c3accd6142a185ee48ef308919
VK-Chase <112039422+vk-chase@users.noreply.github.com>
2024-09-21 02:34:06 -0500

    Update server.lua
    added a remove / add feature to better boost rp

--- server/server.lua
+++ server/server.lua
@@ -14,7 +14,9 @@ local function CreateBicycleItem(bicycle)
     QBCore.Functions.CreateUseableItem(bicycle.model, function(source, item)
         local Player = QBCore.Functions.GetPlayer(source)
         if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-            TriggerClientEvent('c_Bicycle:client:' .. bicycle.name .. 'Menu', source)
+            if Player.Functions.RemoveItem(bicycle.model, 1) then
+                TriggerClientEvent('c_Bicycle:client:' .. bicycle.name .. 'Menu', source)
+            end
         end
     end)
 end
@@ -22,3 +24,17 @@ end
 for _, bicycle in ipairs(bicycles) do
     CreateBicycleItem(bicycle)
 end
+
+RegisterNetEvent('c_Bicycle:server:GiveBikeItem', function(bikeModel)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    Player.Functions.AddItem(bikeModel, 1)
+    TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[bikeModel], 'add')
+end)
+
+RegisterNetEvent('c_Bicycle:server:RemoveBikeItem', function(bikeModel)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    Player.Functions.RemoveItem(bikeModel, 1)
+    TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[bikeModel], 'remove')
+end)
