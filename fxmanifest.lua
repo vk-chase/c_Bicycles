26fc7bd14ec4a01abebd88cda3fb508684f74e47
Chase <112039422+vk-chase@users.noreply.github.com>
2025-09-19 17:54:31 -0500

    Update fxmanifest.lua
    oxlib support

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,13 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'vk-chase'
+fx_version "cerulean"
+game "gta5"
+lua54 'yes'
 
 client_scripts {
-    'client/main.lua'
+'client/**.lua',
 }
-
 server_scripts {
-    'server/main.lua'
+'server/**.lua',
+}
+shared_scripts {
+  '@ox_lib/init.lua',
 }
